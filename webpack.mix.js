9a6f7ee3dbe2330767063ec27009577dc97851b4
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-02-10 16:23:27 -0300

    layout montado

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .copyDirectory('resources/images', 'public/img');
+
+if (mix.inProduction()) {
+    mix.version();
+}
